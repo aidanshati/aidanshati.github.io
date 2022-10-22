d5545f6a778d5c020eec9aa5efe075a2a1941cd5
anony <109928597+aidanshati@users.noreply.github.com>
2022-10-21 23:49:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-function connectWallet() {
-  window.ethereum.request({method: "eth_requestAccounts"}).catch((err)=>{
+async function connectWallet() {
+  accounts = await window.ethereum.request({method: "eth_requestAccounts"}).catch((err)=>{
     //error handling
     console.log(err.code)
   })
+
+  console.log(accounts)
 }
